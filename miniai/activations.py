8bf6e1e313ea3fe6d359b3fb3af772868417705c
ubermenchh <umangkaushik46.bs@gmail.com>
2024-03-07 17:43:55 +0530

    init.py

--- miniai/activations.py
+++ miniai/activations.py
@@ -43,15 +43,15 @@ class HooksCallback(Callback):
     def __init__(self, hookfunc, mod_filter=fc.noop, on_train=True, on_valid=False, mods=None):
         fc.store_attr()
         super().__init__()
-        
+
     def before_fit(self, learn):
         if self.mods: mods = self.mods
         else: mods = fc.filter_ex(learn.model.modules(), self.mod_filter)
         self.hooks = Hooks(mods, partial(self._hookfunc, learn))
-        
+
     def _hookfunc(self, learn, *args, **kwargs):
         if (self.on_train and learn.training) or (self.on_valid and not learn.training): self.hookfunc(*args, **kwargs)
-    
+
     def after_fit(self, learn): self.hooks.remove()
     def __iter__(self): return iter(self.hooks)
     def __len__(self): return len(self.hooks)
@@ -83,7 +83,7 @@ class ActivationStats(HooksCallback):
 
     def dead_chart(self, figsize=(11, 5)):
         fig, axes = get_grid(len(self), figsize=figsize)
-        for ax, h in zip(axes.flatten(), self): 
+        for ax, h in zip(axes.flatten(), self):
             ax.plot(get_min(h))
             ax.set_ylim(0, 1)
 
@@ -92,5 +92,5 @@ class ActivationStats(HooksCallback):
         for h in self:
             for i in 0, 1: axs[i].plot(h.stats[i])
         axs[0].set_title("Means")
-        axs[1].set_title("Stdevs") 
-        plt.legend(fc.L.range(self))                     
+        axs[1].set_title("Stdevs")
+        plt.legend(fc.L.range(self))
